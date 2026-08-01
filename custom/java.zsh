fa366f3868d5523d1bf60a183c49606b5554c1cb
niamot <myemail>
2026-08-02 01:38:03 +0600

    update JAVA_HOME setter
    dynamically sets JAVA_HOME from `which java`

--- custom/java.zsh
+++ custom/java.zsh
@@ -1,6 +1,6 @@
-## JAVA_HOME
-if command -v java &> /dev/null
-then
-	# export JAVA_HOME="$(readlink -f /usr/bin/java | sed "s:/bin/java::")" 
-  export JAVA_HOME="$HOME/.local/share/JetBrains/Toolbox/apps/android-studio/jbr/"
+# Set JAVA_HOME variable
+if command -v java &>/dev/null; then
+	## Get the actual java executable path from update-alternatives
+	export JAVA_HOME=$(readlink -f $(which java) | sed 's:/bin/java::')
+	# export PATH=$JAVA_HOME/bin:$PATH
 fi
