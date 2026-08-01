cbe241c3c8d58b5f2886d20499ecd663ce0e0aee
niamot <myemail>
2026-08-02 01:35:23 +0600

    add alias, update helper func

--- custom/alias.zsh
+++ custom/alias.zsh
@@ -1,17 +1,18 @@
-# Function to alias commands only if they exist
+# alias commands only if they exist
 alias_if_available() {
-    local alias_name=$1
-    local target_command=$2
-    local extra_args=${3:-}
-    
-    if command -v "$target_command" &> /dev/null; then
-        alias "$alias_name"="$target_command $extra_args"
-        return 0
-    else
-        # echo "Note: $target_command not found, skipping alias '$alias_name'"
-        return 1
-    fi
+	local alias_name=$1
+	local target_command=$2
+	local extra_args=${3:-}
+
+	if command -v "$target_command" &>/dev/null; then
+		alias "$alias_name"="$target_command $extra_args"
+		return 0
+	else
+		# TODO: log somewhere instead of direct stdout
+		# echo "Note: $target_command not found, skipping alias '$alias_name'"
+		return 1
+	fi
 }
 
 alias_if_available nano nvim "--clean"
-# alias_if_available unzip unar 
+alias_if_available open 'xdg-open'
