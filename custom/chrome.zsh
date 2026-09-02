6550ba2fd26e1c3a5fb419f01feafd3972057f02
niamot <myemail>
2026-09-03 00:55:38 +0600

    set CHROME_EXECUTABLE variable to the availabe Brave executable

--- custom/chrome.zsh
+++ custom/chrome.zsh
@@ -1,9 +1,13 @@
 # Set Brave as default chromium browser
 ## sets CHROME_EXECUTABLE variable to the availabe Brave executable
-if command -v brave-origin &>/dev/null; then
-	## brave-origin
-	export CHROME_EXECUTABLE="brave-origin"
+if command -v brave &>/dev/null; then
+	## brave
+	export CHROME_EXECUTABLE="brave"
 elif command -v brave-browser &>/dev/null; then
 	## brave-browser
 	export CHROME_EXECUTABLE="brave-browser"
+elif command -v brave-origin &>/dev/null; then
+	## brave-origin
+	export CHROME_EXECUTABLE="brave-origin
+"
 fi
